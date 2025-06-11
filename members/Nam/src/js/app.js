1d3f35977ae63aac8f8daa42552688689b49b1dd
Nam Hoai <nhoaiinamm05@gmail.com>
2025-06-11 12:07:53 +0700

    Remove mistaken submodule and re-add Nam folder

---
+++ members/Nam/src/js/app.js
@@ -0,0 +1,7 @@
+const links = document.querySelectorAll(".nav-link");
+links.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    links.forEach((link) => link.classList.remove("active"));
+    e.target.classList.add("active");
+  });
+});
