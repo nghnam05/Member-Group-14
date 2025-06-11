96fd4c644ee1bcc65bc66947977503546dc4ffb9
Nam Hoai <nhoaiinamm05@gmail.com>
2025-06-11 20:40:41 +0700

    fix portfolio cua Nam

--- members/Nam/src/js/app.js
+++ members/Nam/src/js/app.js
@@ -1,7 +1,37 @@
 const links = document.querySelectorAll(".nav-link");
+const h1 = document.querySelector(".hello-text");
+const title = ['Hello','Everyone','!'];
 links.forEach((link) => {
   link.addEventListener("click", (e) => {
     links.forEach((link) => link.classList.remove("active"));
     e.target.classList.add("active");
   });
 });
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        const progressBars = entry.target.querySelectorAll(".progress-bar");
+        progressBars.forEach((bar) => {
+          const target = +bar.getAttribute("data-target");
+          let count = 0;
+          const interval = setInterval(() => {
+            if (count >= target) {
+              clearInterval(interval);
+            } else {
+              count++;
+              bar.style.width = count + "%";
+              bar.textContent = count + "%";
+            }
+          }, 15); 
+        });
+        observer.unobserve(entry.target); 
+      }
+    });
+  },
+  { threshold: 0.3 }
+);
+
+const skillSection = document.getElementById("skills");
+observer.observe(skillSection);
+
